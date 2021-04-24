5b27b324e5bf72a237c901b8556e6492d2ca1749
adrothemaster <81396222+adrothemaster@users.noreply.github.com>
2021-04-24 15:42:59 -0500

    added one more line of code

--- src/day1/helloWorld.java
+++ src/day1/helloWorld.java
@@ -3,5 +3,6 @@ package day1;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("adding to github");
     }
 }
